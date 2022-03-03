b33b1b7a444b884ac6a2bab46423cb693ca1020d
Nilesh <86908368+nkil123@users.noreply.github.com>
2022-03-03 13:33:02 +0530

    initialize

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 require ('dotenv').config ();
 const app = require ('./app');
-const port = process.env.PORT || 4001;
+const port = process.env.PORT || 4002;
 const connect = require ('./configs/db');
 
 app.listen (port, async (req, res) => {
   await connect ();
-  console.log ('listening to port 4000');
+  console.log ('listening to port 4002');
   console.log ('connected to data base');
 });
--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import {Navbar} from './components/Navbar';
+import {Rout} from './routes/route';
 
-function App() {
+function App () {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Rout />
     </div>
   );
 }
