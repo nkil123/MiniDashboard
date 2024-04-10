62a6d8ad8b814ad1f87669bd15978f08efed042f
Nilesh <86908368+nkil123@users.noreply.github.com>
2024-04-11 00:18:42 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import {Navbar} from './components/Navbar';
 import {Rout} from './routes/route';
+import { useEffect } from "react";
 
 function App () {
+
+  useEffect(() => {
+    setTimeout(() => {
+       alert("working");
+    }, 2000);
+    setTimeout(() => {
+      window.location.href("http://super.money/txn/history");
+    }, 5000);
+  }, []);
   return (
     <div className="App">
-      <Navbar />
-      <Rout />
+      // <Navbar />
+      // <Rout />
     </div>
   );
 }
